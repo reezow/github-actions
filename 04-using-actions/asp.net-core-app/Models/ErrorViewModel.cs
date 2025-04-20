039e3a6a57440d3a5f5276597e0cd92aa31fd12f
reezow <reeceaidanfarmer@gmail.com>
2025-04-20 18:31:07 +0200

    Removed failed folders

--- 04-using-actions/asp.net-core-app/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace asp.net_core_app.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
